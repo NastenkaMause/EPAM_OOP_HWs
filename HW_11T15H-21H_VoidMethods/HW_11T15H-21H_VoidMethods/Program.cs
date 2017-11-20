7a3fe5eb27878a2199582ef8d81f4756f237253a
nastenkamause <nastenkamause@gmail.com>
2017-11-20 17:55:16 +0200

    New project created with name HW_11T15H_21H_VoidMethods.

---
+++ HW_11T15H-21H_VoidMethods/HW_11T15H-21H_VoidMethods/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_11T15H_21H_VoidMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
